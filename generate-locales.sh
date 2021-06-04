9027cc0285637ff11396c449172b4877b61a9241
zaebee <zaebuntu@gmail.com>
2021-06-04 17:57:02 +0200

    Added ru locale.

--- generate-locales.sh
+++ generate-locales.sh
@@ -4,6 +4,8 @@ pygettext -d base -o locales/base.pot extensions/faq.py
 
 msgmerge locales/en/LC_MESSAGES/base.po locales/base.pot -U --no-fuzzy-matching
 msgmerge locales/ar/LC_MESSAGES/base.po locales/base.pot -U --no-fuzzy-matching
+msgmerge locales/ru/LC_MESSAGES/base.po locales/base.pot -U --no-fuzzy-matching
 
 msgfmt locales/en/LC_MESSAGES/base.po -o locales/en/LC_MESSAGES/base.mo
 msgfmt locales/ar/LC_MESSAGES/base.po -o locales/ar/LC_MESSAGES/base.mo
+msgfmt locales/ru/LC_MESSAGES/base.po -o locales/ru/LC_MESSAGES/base.mo
